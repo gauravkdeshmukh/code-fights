e410a55d15a167e0024f77b4fa32e680cea63eb4
GAURAV  DESHMUKH <gauravkdeshmukh@users.noreply.github.com>
2018-05-21 17:54:00 -0700

    Update nextSecond.js

--- nextSecond.js
+++ nextSecond.js
@@ -1,3 +1,28 @@
+/*
+Given some time output the time after one second.
+
+Example
+
+For currentTime = [23, 59, 59], the output should be
+nextSecond(currentTime) = [0, 0, 0].
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.integer currentTime
+
+Current time represented by array of three integers as follows: [hours, minutes, second].
+
+The time is guaranteed to be valid, i.e.:
+0 ≤ hours ≤ 23,
+0 ≤ minutes, seconds ≤ 59.
+
+[output] array.integer
+
+Time after one second in the same format.
+*/
+
 function nextSecond(currentTime) {
   return [(currentTime[0]%23 !==0 && currentTime[1]%59 === 0) ? currentTime[0]+1: currentTime[0]%23,
           (currentTime[1]%59 !==0 && currentTime[2]%59 === 0) ? currentTime[1]+1: currentTime[1]%59,
